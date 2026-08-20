ced8de90d8495b82b4c51c58b74902d01426be52
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-08-20 23:32:43 +0530

    chore: type Telegram icon decompressor

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,7 @@
+declare module 'pako' {
+  export function ungzip(data: Uint8Array, options?: {to?: 'string'}): string | Uint8Array;
+}
+
 declare global {
   interface Window {
     Telegram?: {WebApp?: any};
