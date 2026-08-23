c11048efc4422b29f30b8c14f7353ab593f26109
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-08-23 11:13:46 +0530

    Remove final TADS provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {TadsWidgetProvider} from 'react-tads-widget';
 import App from './App';
 import './styles.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><TadsWidgetProvider><App/></TadsWidgetProvider></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
