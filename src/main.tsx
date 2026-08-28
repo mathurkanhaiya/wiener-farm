064cc7b771b67e2f7431c0697e3e97dac663aa17
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-08-28 23:15:05 +0530

    Translate existing screens through safe display layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import {I18nProvider} from './i18n';
+import {LocalizedSurface} from './LocalizedSurface';
 import './styles.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><I18nProvider><App/></I18nProvider></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><I18nProvider><LocalizedSurface/><App/></I18nProvider></React.StrictMode>);
