068955078d950e421adcfe94fcb07c6217d5a66e
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-09-02 23:01:33 +0530

    Route Telegram webhook bridge through VPS port 8080

--- api/telegram-webhook.js
+++ api/telegram-webhook.js
@@ -1,4 +1,4 @@
-const VPS='http://15.235.145.222';
+const VPS='http://15.235.145.222:8080';
 export default async function handler(req,res){
   if(req.method!=='POST') return res.status(200).send('ok');
   try{
