daf131dc0bde83f87b62a97c857fffe668eada29
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-09-03 23:44:01 +0530

    Wrap Mini App with crash recovery and haptic layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App';
 import {I18nProvider} from './i18n';
 import {LocalizedSurface} from './LocalizedSurface';
+import {StabilityLayer} from './StabilityLayer';
 import './styles.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><I18nProvider><LocalizedSurface/><App/></I18nProvider></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><I18nProvider><StabilityLayer><LocalizedSurface/><App/></StabilityLayer></I18nProvider></React.StrictMode>);
