9c04ecbf9f3c6d6846e2cce085bfff4283922dcd
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-09-06 09:24:23 +0530

    Enable clean Spin launcher card styling

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,5 @@ import {I18nProvider} from './i18n';
 import {LocalizedSurface} from './LocalizedSurface';
 import {StabilityLayer} from './StabilityLayer';
 import './styles.css';
+import './styles-spin-card-clean.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><I18nProvider><StabilityLayer><LocalizedSurface/><App/></StabilityLayer></I18nProvider></React.StrictMode>);
