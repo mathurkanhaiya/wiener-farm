e364469fe8f46a37199ba6d72ac9a7b704e9d80c
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-09-08 20:40:06 +0530

    Merge V75 complete translation packs

---
+++ src/i18n-v75.ts
@@ -0,0 +1,6 @@
+import type {V75Pack} from './i18n-v75-keys';
+import {V75_A} from './i18n-v75-a';
+import {V75_B} from './i18n-v75-b';
+import {V75_C} from './i18n-v75-c';
+import {V75_D} from './i18n-v75-d';
+export const V75_PACKS:Record<string,V75Pack>={...V75_A,...V75_B,...V75_C,...V75_D};
