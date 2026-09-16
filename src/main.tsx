337a32267d3174cd16c76182f0ac1f855640d497
mathurkanhaiya <156588144+mathurkanhaiya@users.noreply.github.com>
2026-09-16 07:49:22 +0530

    Use guest mode outside Telegram instead of login gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
+import GuestApp from './GuestApp';
 import {I18nProvider} from './i18n';
 import {LocalizedSurface} from './LocalizedSurface';
 import {StabilityLayer} from './StabilityLayer';
@@ -9,4 +10,17 @@ import './styles.css';
 import './styles-spin-card-clean.css';
 import './styles-spin-notifications.css';
 import './nav-six.css';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><I18nProvider><StabilityLayer><LocalizedSurface/><SpinNotificationLayer/><App/></StabilityLayer></I18nProvider></React.StrictMode>);
+
+const hasTelegramSession=()=>Boolean(window.Telegram?.WebApp?.initData);
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <I18nProvider>
+      <StabilityLayer>
+        <LocalizedSurface/>
+        <SpinNotificationLayer/>
+        {hasTelegramSession()?<App/>:<GuestApp/>}
+      </StabilityLayer>
+    </I18nProvider>
+  </React.StrictMode>
+);
